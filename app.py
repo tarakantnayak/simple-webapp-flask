dacf6cd4f1257aafe8c908b1ffe87b4a265ebf40
Tarakant Nayak <tarakant@gmail.com>
2020-03-03 10:02:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+app = Flask(_name_)
+
+@app.route("/")
+def main():
+	retrun "Welcome Budy!"
+	
+@app.route("/how are you")
+def hello():
+	return "I am good, how about you?"
+	
+if _name_ == "_main_":
+	app.run()
